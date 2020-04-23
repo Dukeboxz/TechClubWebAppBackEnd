22f0de8c4fa0f142d7273f1376e90ad0146442e0
Dukeboxz <u04sjj@gmail.com>
2020-04-23 12:00:01 +0100

    testing connection changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ const memberRouter = require('./routers/member')
 const badgeRouter = require('./routers/badge')
 
 const app = express()
+
+app.use(function(req, res, next){
+    console.log('In app use')
+    res.header("Access-Control-Allow-Origin", "*" );
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+
 app.use(bodyParser.json())
 
 app.use(memberRouter);
--- src/routers/member.js
+++ src/routers/member.js
@@ -3,6 +3,21 @@ const router = new express.Router();
 
 const Member = require('../models/member')
 
+router.get('/members', async (req, res)=> {
+
+    try{
+        console.log("in /members")
+        const all = await Member.find();
+        console.log(all);
+        res.status(200).send(all);
+    
+    }catch(e){
+        console.log(e)
+        res.status(400).send(e)
+    }
+
+})
+
 router.post('/member', async (req, res) =>{
 
    
@@ -10,8 +25,8 @@ router.post('/member', async (req, res) =>{
     console.log(member)
     try{
         await member.save()
-
-        res.status(201).send(req)
+        console.log('AFTER SAVE')
+        res.status(201).send()
     }catch(e){
         console.log(e)
         res.status(400).send(e)
